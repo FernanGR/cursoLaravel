4ee8eb3602b54e98e70292207683663cda18ada2
Fernando Garcia Rubio <tibofgr@gmail.com>
2020-09-12 06:11:42 +0200

    38

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -10,11 +10,11 @@
     @if($errors->any())
       <div class="alert alert-danger">
         <h6>Por favor corrige los errores de abajo: </h6>
-      {{-- <ul>
+      <ul>
         @foreach ($errors->all() as $error)
             <li> {{ $error }}</li>
           @endforeach
-      </ul> --}}
+      </ul>
     </div>
 
     @endif
@@ -25,27 +25,15 @@
 
         <label for="name">Nombre:</label>
         <input type="text" name="name" value="{{ old('name') }}" id="name" placeholder="Pedro Perez">
-        <div class="text-danger">
-          @if ($errors->has('name'))
-            <p>{{ $errors->first('name') }}</p>
-          @endif
-        </div>
+
         <br>
         <label for="email">Correo electrónico:</label>
         <input type="email" name="email" value="{{ old('email') }}" id="email" placeholder="Pedro@example.com">
-        <div class="text-danger">
-          @if ($errors->has('email'))
-            <p>{{ $errors->first('email') }}</p>
-          @endif
-        </div>
+
         <br>
         <label for="password">Contraseña:</label>
         <input type="password" name="password" value="" id="password" placeholder="Mayor a 6 caracteres">
-        <div class="text-danger">
-          @if ($errors->has('password'))
-            <p>{{ $errors->first('password') }}</p>
-          @endif
-        </div>
+ 
         <br>
         <button type="submit">Crear usuario</button>
 
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,12 +4,22 @@
 
 @section('content')
     <h1>{{ $title }}</h1>
-
+    <p>
+      <a href="{{ route('users.create') }}">Nuevo usuario</a>
+    </p>
     <ul>
         @forelse ($users as $user)
             <li>
                 {{ $user->name }}, ({{ $user->email }})
-                <a href="{{ route('users.show', ['id' => $user->id]) }}">Ver detalles</a>
+                <a href="{{ route('users.show',  $user) }}">Ver detalles</a> |
+                <a href="{{ route('users.edit',  $user) }}">Editar</a> |
+                <form  action="{{ route('users.destroy',  $user) }}" method="POST">
+                  {{ csrf_field() }}
+                  {{ method_field('DELETE') }}
+                  <button type="submit" name="button">Eliminar</button>
+                </form>
+
+
             </li>
         @empty
             <li>No hay usuarios registrados.</li>
